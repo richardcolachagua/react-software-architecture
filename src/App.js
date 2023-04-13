1800e1fbbc0885db8351fc3da1f900d2ee32953c
Richard Colachagua <richardcolachagua@gmail.com>
2023-04-13 19:27:15 -0400

    "React Software Architecture"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link, Routes } from "react-router-dom";
+import styled from "styled-components";
+import Home  from "./pages/Home"
+import About  from "./pages/About"
+import  Articles  from "./pages/Articles";
 
-function App() {
+const BigGreenHeading = styled.h1`
+    color: green;
+    font-size: 96px;
+    `;
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BigGreenHeading>Server-Side Rendering Example</BigGreenHeading>
+    <ul>
+    <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/articles">Articles</Link>
+      </li>
+    </ul>
+    <Routes>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/about" exact>
+        <About />
+      </Route>
+      <Route path="/articles" exact>
+        <Articles />
+      </Route>
+    </Routes>
+      </>
+  )
 }
 
 export default App;
